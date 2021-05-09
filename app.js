678e41390754afa2ed94f6ed13c092f3b6bb817d
Mercy Baffour <mercy.baffour@gmail.com>
2021-05-08 21:20:30 -0400

    first styling and initial code

---
+++ app.js
@@ -0,0 +1,13 @@
+//Getting longitude and latitude from built-in function
+window.addEventListener ('load', () => {
+    let long;
+    let lat;
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+        });
+    }
+
+});
